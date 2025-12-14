2524d81bf3791ed41836ddfaaf3990f91e0258ba
nirajpatel518513-cell <nirajpatel518513@gmail.com>
2025-12-15 02:39:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Your repository name is 'a-carder'
+  base: "/a-carder/", 
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
-});
+})
